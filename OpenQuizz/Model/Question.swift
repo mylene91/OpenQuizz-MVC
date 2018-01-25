551babc849087b1feabafe0d64c1050e8d20f3c9
Mylene91 <mylene.greneron@gmail.com>
2018-01-25 09:33:06 +0100

    ajout fichier Question.swift

---
+++ OpenQuizz/Model/Question.swift
@@ -0,0 +1,16 @@
+//
+//  Question.swift
+//  OpenQuizz
+//
+//  Created by Mylène G on 24/01/2018.
+//  Copyright © 2018 Mylene. All rights reserved.
+//
+
+import Foundation
+
+
+class Question {
+    var title = ""
+    var isCorrect = false 
+    
+}
