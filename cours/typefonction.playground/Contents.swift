0959f6828b07d93b6c6ff82c9fb5039ad4f6208a
Mylene91 <mylene.greneron@gmail.com>
2018-01-29 22:54:32 +0100

    exo sur les types fonctions

--- cours/typefonction.playground/Contents.swift
+++ cours/typefonction.playground/Contents.swift
@@ -2,28 +2,7 @@
 
 import UIKit
 
-var str = "Hello, playground"
 
-struct Adresse {
-    var voie: String
-    var ville: String
-    var pays: String
-    var codePostal: String
-    
-    mutating func Afficher() {
-        voie = "3 imp des canuts"
-        ville = "corbas"
-        pays = "France"
-        codePostal = "69330"
-    }
-    
-}
-
-var monAdresse = Adresse(voie: "3 impasse", ville: "Lyon", pays: "France", codePostal: "6990")
-print(monAdresse)
-
-var monAdresse2 = Adresse(voie: "ef", ville: "kejfk", pays: "nnj", codePostal: "9999")
-print(monAdresse2)
 
 
 ///// TYPES FONCTION ///
@@ -56,8 +35,44 @@ for maFonction in mesFonctions {
 }
  a // va valloir 16 car la variable est passée par plusieurs étapes des fonctions dans le tableau
 
+// Exo : Dans cet exercice, on cherche à calculer la somme d'un tableau d'entier selon les règles suivantes :
+
+//si le nombre est pair, on le divise par deux avant de l'additionner aux autres
+//si le nombre est impair, on ajoute un puis on le divise par deux avant de l'additioner aux autres
+
+//Vous devez compléter la fonction obtenirDivision. Cette fonction renvoie une des deux fonctions au
+//dessus en fonction de la parité de son paramètre x. Vous noterez que le type de retour de cette
+//fonction corresponds bien au type des deux fonctions du dessus. Ensuite, vous devez utiliser la
+//fonction obtenirDivision pour compléter la boucle
+
+func diviserNombrePairParDeux(x: Int) -> Int {
+    return x / 2
+}
+
+func diviserNombreImpairParDeux(x: Int) -> Int {
+    return (x + 1) / 2
+}
+
+// renvoie une des deux fonctions au dessus (si c'est entier ou non)
+func obtenirDivision(x: Int) -> (Int) -> (Int) {
+    if (x % 2 == 0) {//si paramètre x est égal à un entier (modulo 2 = pair)
+       return diviserNombrePairParDeux(x: ) // pas de paramètre car il va prendre celui du tableau
+    } else { // sinon renvoi à la fonction pour les nombres impairs
+        return diviserNombreImpairParDeux(x: )
+    }
+}
+
+let tableau = [2, 12, 3, 14, 76, 19, 7, 22]
+var somme = 0
+var nouveauTableau : [Int] = []
 
 
+for nombre in tableau {
+    let laDivision = obtenirDivision(x: nombre)
+    laDivision(nombre)
+    somme += laDivision(nombre)
+    
+}
 
 
 
