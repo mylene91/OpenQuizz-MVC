0570974d7ecc8d169f883c3a71fb918cf78656c2
Mylene91 <mylene.greneron@gmail.com>
2018-01-29 23:00:14 +0100

    ajout test pour vérifier que receivequestions fonctionne et affiche les questions/reponses dans la console

--- OpenQuizz/Supporting Files/AppDelegate.swift
+++ OpenQuizz/Supporting Files/AppDelegate.swift
@@ -16,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let game = Game() // test lors du lancement pour check si receivequestions fonctionne
+        game.refresh()
         return true
     }
 
