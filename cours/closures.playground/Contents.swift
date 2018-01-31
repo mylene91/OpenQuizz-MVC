fdc6735a7b1564050d0f2eb8c8c5bbe97cc88d71
Mylene91 <mylene.greneron@gmail.com>
2018-01-31 15:41:14 +0100

    ajout exo closures

--- cours/closures.playground/Contents.swift
+++ cours/closures.playground/Contents.swift
@@ -13,3 +13,41 @@ func multiplier (a: Int, b: Int) -> Int {
 { (a: Int, b: Int) -> Int in // { + paramètres et val de retours + in + corps de la func }
     return a * b
 }
+
+// faire des requêtes sur le réseau (attendre le retour des données)
+// de manière plus générale : effectuer une action que l'on ignore à un moment précis : en prend une fermeture pour laisser à l'utilisateur la possibilité de choisir l'action qu'il veut réaliser
+// pour l'animation
+
+// quand la fermeture n'a pas de valeur de retour, on peut l'omettre donc enlever le -> ()
+
+// EXO : Transformez les déclarations de fonctions suivantes en fermetures :
+
+func ajouterDeux(a: Int) -> Int { (...) }
+func additioner(a: Int, b: Int) -> Int { (...) }
+func envoyerMail(message: String, destinataire: String) -> Bool { (...) }
+func cocherLaCase(aCoché: Bool) { (...) }
+func verrouiller() { (...) }
+func composerNumero(_ numero: Int) { (...) }
+
+{ (a: Int) -> Int in
+}
+
+{ (a: Int, b: Int) -> Int in
+}
+
+{ (message: String, destinataire: String) -> Bool in
+}
+
+{ (aCoché: Bool) in
+}
+
+{  // on peut mettre () -> Void
+    
+}
+
+{ (numero: Int) in
+}
+
+
+
+
