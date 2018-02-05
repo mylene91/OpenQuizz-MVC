0f00538472f7d6bd9669a1f7f9f4bac002df792f
Mylene91 <mylene.greneron@gmail.com>
2018-02-05 09:49:33 +0100

    ajout cours sur notifications

---
+++ cours/notifications.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+import Foundation
+
+// Notifications : du frameworks foundation
+
+// notification.Name (id, son émission)
+// notification
+// notificationCenter (emettre et recevoir les notifications)
