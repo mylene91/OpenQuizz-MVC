71eb542c03be3e38da97661002587929472e2c9b
Mylène <mylene.greneron@gmail.com>
2018-02-07 22:43:44 +0100

    ajout cours view

--- cours/notifications.playground/Contents.swift
+++ cours/notifications.playground/Contents.swift
@@ -9,3 +9,20 @@ import Foundation
 // notification.Name (id, son émission)
 // notification
 // notificationCenter (emettre et recevoir les notifications)
+
+
+// (on peut créer nos vues dans l'interface builder mais des fois on va devoir rentrer dans le code avec UIView : Si on veut controler la hiérarchie des vues dans le code on utilisera :
+
+// Propriétés :
+var superview: UIView // permet d'accéder à la vue parente
+var subviews: [UIView] // permet d'accéder au tableau des sous vues qui sont contenues à l'intérieur -> attention, l'ordre importe : les vues qui sont à la fin du tableau sont au dessus de la pile (visuellement)
+
+// par exemple je veux ajouter un bouton : ajouter un
+// Méthodes :
+func addSubview(_view: UIView)
+// ex : je veux ajouter un bouton dans une sous vue (myView serait ma vue)
+myView.addSubview(mybutton)
+
+// ex : je veux retirer le bouton de la sous vue :
+func removeFromSuperview()
+myButton.removeFromSuperview()
