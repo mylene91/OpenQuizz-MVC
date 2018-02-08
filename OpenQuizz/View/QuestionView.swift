b0c8145a78febe5b57511a75a8e0bee92457f115
Mylene G <mylene.greneron@gmail.com>
2018-02-08 11:36:34 +0100

    ajout de la classe QuestionView qui a pour but de simplifier l'utilisation de la vue : ajout de deux outlet avec l'icon et label

---
+++ OpenQuizz/View/QuestionView.swift
@@ -0,0 +1,29 @@
+//
+//  QuestionView.swift
+//  OpenQuizz
+//
+//  Created by Mylène Greneron on 08/02/2018.
+//  Copyright © 2018 Mylene. All rights reserved.
+//
+
+import UIKit
+
+// simplifier l'utilisation de la vue
+class QuestionView: UIView {
+    
+    // @IBOutlet va notifier xcode pour lui indiquer que ces propriétés son susbetibles de faire une connexion avec une vue ans un interfacebuilder
+    
+    // connexion avec label
+    @IBOutlet private var label: UILabel? // utiliser des optionnels car on ne connait pas les valeurs de ses propriétés
+    
+    // connexion avec Icon
+    @IBOutlet private var icon: UIImageView?
+    /*
+    // Only override draw() if you perform custom drawing. (permet de dessiner des vues de toutes formes)
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
