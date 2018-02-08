eee52181930afff41340db125da4f0295c8e038d
Mylene G <mylene.greneron@gmail.com>
2018-02-08 11:47:20 +0100

    modification déballage de l'optionnelé

--- OpenQuizz/View/QuestionView.swift
+++ OpenQuizz/View/QuestionView.swift
@@ -14,10 +14,10 @@ class QuestionView: UIView {
     // @IBOutlet va notifier xcode pour lui indiquer que ces propriétés son susbetibles de faire une connexion avec une vue ans un interfacebuilder
     
     // connexion avec label
-    @IBOutlet private var label: UILabel? // utiliser des optionnels car on ne connait pas les valeurs de ses propriétés
+    @IBOutlet private var label: UILabel! // utiliser des optionnels car on ne connait pas les valeurs de ses propriétés -> on utilise par contre un optionnel déballé car on est sûrs que ça ne contiendra pas nil
     
     // connexion avec Icon
-    @IBOutlet private var icon: UIImageView?
+    @IBOutlet private var icon: UIImageView!
     /*
     // Only override draw() if you perform custom drawing. (permet de dessiner des vues de toutes formes)
     // An empty implementation adversely affects performance during animation.
