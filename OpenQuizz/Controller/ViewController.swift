c60fc49e97d6b66faf05bf9e3a136d74ee6b5a0a
Mylene G <mylene.greneron@gmail.com>
2018-02-20 14:43:52 +0100

    ajout outlet newGameButton, activityIndicator, scoreLabel, questionView

--- OpenQuizz/Controller/ViewController.swift
+++ OpenQuizz/Controller/ViewController.swift
@@ -10,16 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var newGameButton: UIButton!
+    
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    
+    @IBOutlet weak var questionView: QuestionView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
+    
 
 }
 
